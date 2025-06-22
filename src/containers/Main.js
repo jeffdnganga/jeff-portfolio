23017f9d21d24d6e55d01607e1282eee7c61ca69
Jeff Ng'ang'a <jeffdnganga@gmail.com>
2025-06-23 02:50:07 +0300

    Fix duplicate key 'image' in Talks.js and non rendering section after Talks

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -19,6 +19,7 @@ import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
 import {StyleProvider} from "../contexts/StyleContext";
 import {useLocalStorage} from "../hooks/useLocalStorage";
+import { podcastSection } from "../portfolio";
 import "./Main.scss";
 
 const Main = () => {
@@ -62,7 +63,7 @@ const Main = () => {
             <Blogs />
             <Talks />
             <Twitter />
-            <Podcast />
+            {podcastSection.display && <Podcast />}
             <Profile />
             <Footer />
             <ScrollToTopButton />
