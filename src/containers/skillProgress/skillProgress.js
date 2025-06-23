949eacfb83aeb8939401965f4084a3ad0f448bf3
Jeff Ng'ang'a <jeffdnganga@gmail.com>
2025-06-23 03:26:21 +0300

    Fixed alignment of Proficiency section

--- src/containers/skillProgress/skillProgress.js
+++ src/containers/skillProgress/skillProgress.js
@@ -9,33 +9,35 @@ export default function StackProgress() {
   if (techStack.viewSkillBars) {
     return (
       <Fade bottom duration={1000} distance="20px">
-        <div className="skills-container">
-          <div className="skills-bar">
-            <h1 className="skills-heading">Proficiency</h1>
-            {techStack.experience.map((exp, i) => {
-              const progressStyle = {
-                width: exp.progressPercentage
-              };
-              return (
-                <div key={i} className="skill">
-                  <p>{exp.Stack}</p>
-                  <div className="meter">
-                    <span style={progressStyle}></span>
+        <div className="main">
+          <div className="skills-container">
+            <div className="skills-bar">
+              <h1 className="skills-heading">Proficiency</h1>
+              {techStack.experience.map((exp, i) => {
+                const progressStyle = {
+                  width: exp.progressPercentage
+                };
+                return (
+                  <div key={i} className="skill">
+                    <p>{exp.Stack}</p>
+                    <div className="meter">
+                      <span style={progressStyle}></span>
+                    </div>
                   </div>
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
 
-          <div className="skills-image">
-            {illustration.animated ? (
-              <DisplayLottie animationData={Build} />
-            ) : (
-              <img
-                alt="Skills"
-                src={require("../../assets/images/skill.svg")}
-              />
-            )}
+            <div className="skills-image">
+              {illustration.animated ? (
+                <DisplayLottie animationData={Build} />
+              ) : (
+                <img
+                  alt="Skills"
+                  src={require("../../assets/images/skill.svg")}
+                />
+              )}
+            </div>
           </div>
         </div>
       </Fade>
